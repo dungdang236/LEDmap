8c196b693f0f26cc81258acd6e6d924884775fc7
student <student@techmasters-imac-2.local>
2015-11-27 14:40:26 -0800

    Run led controller

--- LEDmap/ViewController.m
+++ LEDmap/ViewController.m
@@ -17,30 +17,71 @@
 {
     CGFloat _WMargin;
     CGFloat _HMargin;
-    int _numberOfBall;
+    int _numberOfBallInARow;
+    int _numberOfBallInACollum;
     CGFloat _space;
     CGFloat _ballDiameter;
     BOOL check;
+    NSTimer* _timer;
+    int lastonLEDc;
+    int lastonLEDr;
 }
 - (void)viewDidLoad {
     [super viewDidLoad];
-    _WMargin= 40;
+    _WMargin= 60;
     _HMargin =200;
     _ballDiameter=24;
+    _numberOfBallInARow=10;
+    _numberOfBallInACollum=14;
+    lastonLEDc =-1;
+    lastonLEDr =-1;
     //[self placeBallAtX:200 andY:200 withTag:1];
     [self checkSizeOfApp];
     [self numberOfBallvsSpace];
     
     
     
+    
+    
+}
+- (void) runningLED{
+    if(lastonLEDc!=-1||lastonLEDr!=-1){
+        [self turnOffLED:lastonLEDc];
+        [self turnOffLED:lastonLEDr];
+    }
+    if (lastonLEDr!= _numberOfBallInARow-1||lastonLEDc!= _numberOfBallInACollum-1) {
+        lastonLEDc++;
+        lastonLEDr++;
+    } else{
+        lastonLEDc=0;
+        lastonLEDr=0;
+    }
+    [self turnOnLED:lastonLEDc andColor:check];
+    [self turnOnLED:lastonLEDr andColor:check];
+}
+- (void) turnOnLED: (int) index andColor: (bool) color{
+    UIView* view =[self.view viewWithTag:index +100];
+    if(view && [view isMemberOfClass:[UIImageView class]]){
+        UIImageView* ball = (UIImageView*) view;
+        ball.image =[UIImage imageNamed:@"green"];
+    }
+                   
+}
+
+- (void) turnOffLED: (int) index{
+    UIView* view =[self.view viewWithTag:index +100];
+    if(view && [view isMemberOfClass:[UIImageView class]]){
+        UIImageView* ball = (UIImageView*) view;
+        ball.image =[UIImage imageNamed:@"blue"];
+    }
+    
 }
 - (IBAction)turnOnOff:(id)sender {
     NSLog(@"Hello");
     //UIImageView* ball=[[UIImageView alloc] initWithImage:[UIImage imageNamed: @"blue"]];
     check = !check;
-        [self drawWightBall:10 and:14 ballkind:check];
-    
-  
+        [self drawWightBall:_numberOfBallInACollum and:_numberOfBallInARow ballkind:check];
+    _timer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(runningLED) userInfo:nil repeats:true];
     
     
     
@@ -54,13 +95,13 @@
               ballkind: (bool)n{
     CGFloat wspace = [self widthspaceBetweenBallCenterWhenNumberOfBallIsKnown:numberOfBallInARow];
     CGFloat hspace = [self heightSpaceBetweenBallCenterWhenNumberOFBallIsKnown:numberOfBallInACollum];
-    for (int i=0; i<numberOfBallInARow; i++) {
-        for (int j=0; j<numberOfBallInACollum; j++) {
+    for (int i=0; i<numberOfBallInACollum; i++) {
+        for (int j=0; j<numberOfBallInARow; j++) {
             
         
             [self placeBallAtX:_WMargin + i*wspace
                           andY:_HMargin +j*hspace
-                       withTag:i+j+100
+                       withTag: i+j*numberOfBallInACollum+100
                         ballkind:n];
             
         }
